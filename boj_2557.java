15d36e38ffce07f22c1126fdb92ac8f658a8db6a
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2020-12-30 15:04:36 +0900

    2557 Hello World
    2557 Hello World

---
+++ boj_2557.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class boj_2557 {
+
+	public static void main(String[] args) throws IOException
+	{
+		System.out.println("Hello World!");
+	}
+
+}
