4eaa68af54e7f9abda4d398f875d248b13d8a717
tjdgh <71371075+tlatjdgh3778@users.noreply.github.com>
2020-12-30 17:52:37 +0900

    1001(A-B)

---
+++ Step1/BOJ_1001.java
@@ -0,0 +1,18 @@
+package Step1;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class BOJ_1001
+{
+	public static void main (String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int sum = A - B;
+		System.out.println(sum);
+	}
+}
